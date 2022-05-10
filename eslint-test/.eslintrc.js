e3e873bcc5aa4a0d6a90e407975168ad4b65fcba
productuidev <productuidev@gmail.com>
2022-05-11 06:56:24 +0900

    install eslint

---
+++ eslint-test/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        semi: ["error", "always"],
+    }
+}
